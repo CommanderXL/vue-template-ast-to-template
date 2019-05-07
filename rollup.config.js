58c7f855ac109cde9a8d2a1584c66a1004c485a7
xiaolei <1017653702@qq.com>
2019-05-07 10:34:10 +0800

    [update]更新打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript'
 import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
 
 export default {
   input: './lib/index.ts',
   output: {
-    file: './dist/index.js',
+    file: pkg.main,
     format: 'cjs'
   },
   plugins: [resolve(), commonjs(), typescript()]
